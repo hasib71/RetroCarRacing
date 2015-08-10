d807d0e1aeb84ad7373ec802d28db9099b6a50e2
Hasib Al Muhaimin <himuhasib@gmail.com>
2015-08-10 22:09:38 +0600

    Initial GameBoard and settings

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import javax.swing.JFrame;
+
+import com.sadakhata.retrocarracing.*;
+
 
 public class Main {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		JFrame window = new JFrame();
+		
+		GameBoard gb = new GameBoard();
+		
+		window.add(gb);
+		
+		window.setSize(Settings.WINDOW_HEIGHT, Settings.WINDOW_WIDTH);
+		window.setLocationRelativeTo(null);
+		window.setTitle(Settings.GAME_TITLE);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		
 	}
 
 }
---
+++ src/com/sadakhata/retrocarracing/Settings.java
@@ -0,0 +1,9 @@
+package com.sadakhata.retrocarracing;
+
+public class Settings {
+	public static final String GAME_TITLE = "Retro Car Racing";
+	public static final int WINDOW_HEIGHT = 600,
+			WINDOW_WIDTH = 400;
+
+	
+}
