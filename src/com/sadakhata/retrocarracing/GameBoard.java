801c1f9b7ded5d78fc41c6ea9239a2bf420ea759
Hasib Al Muhaimin <himuhasib@gmail.com>
2015-08-10 22:14:46 +0600

    a few update of gameboard

--- src/com/sadakhata/retrocarracing/GameBoard.java
+++ src/com/sadakhata/retrocarracing/GameBoard.java
@@ -1,7 +1,39 @@
 package com.sadakhata.retrocarracing;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 
-public class GameBoard extends JPanel {
+public class GameBoard extends JPanel implements KeyListener {
+	
+	
+	public GameBoard()
+	{
+		super();
+		
+		
+		
+		addKeyListener(this);
+		setFocusable(true);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
